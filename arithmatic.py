1b41ba96e8ec7b83e366d147b9fb35b1b2b868d5
Sachin Ramesh Tidke <sachintidke551@gmail.com>
2025-01-25 14:22:15 +0530

    Add files via upload
    arithmatic

---
+++ arithmatic.py
@@ -0,0 +1,8 @@
+a = int(input("enter a num1:"))
+b = int(input("enter a num2:"))
+
+#print arithmatic operation
+print("addition:",a+b)
+print("substraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
